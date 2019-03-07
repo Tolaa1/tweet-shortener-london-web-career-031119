6d125faebc6c5f9d93a452040455a71c1dc91d43
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-07 09:35:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,15 @@
 # Write your code here.
 def dictionary
   dictionary = {
+    "hello" =>'hi'
+"to" =>2
+"two" 
+too"  '2' 
+"for,
+four"  '4'
+'be' becomes 'b'
+'you' becomes 'u'
+"at" becomes "@" 
+"and" becomes "&"
     
   }
